2f877c519ed660baad8b7893b4214937cf1ae6c6
gremble0 <hermanstornes@gmail.com>
2024-08-29 13:54:44 +0200

    Write select statements

--- sql/films/select-films.sql
+++ sql/films/select-films.sql
@@ -5,14 +5,37 @@ SELECT * FROM films;
 SELECT * FROM films ORDER BY score;
 
 -- All films ordered by release year ascending
+SELECT * FROM films ORDER BY score DESC;
+
 -- All films with a rating of 8 or higher
+SELECT * FROM films WHERE score >= 8;
+
 -- All films with a rating of 7 or lower
+SELECT * FROM films WHERE score <= 7;
+
 -- films released in 1990
+SELECT * FROM films WHERE release_year = 1990;
+
 -- films released before 2000
+SELECT * FROM films WHERE release_year < 2000;
+
 -- films released after 1990
+SELECT * FROM films WHERE release_year > 1990;
+
 -- films released between 1990 and 1999
+SELECT * FROM films WHERE release_year BETWEEN 1990 AND 1999;
+
 -- films with the genre of "SciFi"
+SELECT * FROM films WHERE genre = 'SciFi';
+
 -- films with the genre of "Western" or "SciFi"
+SELECT * FROM films WHERE genre = 'SciFi' OR genre = 'Western';
+
 -- films with any genre apart from "SciFi"
+SELECT * FROM films WHERE genre != 'SciFi' AND genre != 'Western';
+
 -- films with the genre of "Western" released before 2000
+SELECT * FROM films WHERE genre = 'Western' AND release_date < 2000;
+
 -- films that have the world "Matrix" in their title
+SELECT * FROM films WHERE title LIKE '%Matrix%';
