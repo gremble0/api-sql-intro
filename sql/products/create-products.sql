0fae26ce5b3e9a364bfe94dbc1aa1e219b22c14a
gremble0 <hermanstornes@gmail.com>
2024-08-29 14:40:10 +0200

    Remove products

--- sql/products/create-products.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE products(
- id serial PRIMARY KEY,
- name varchar(255) NOT null,
- price int NOT null,
- discount boolean,
- UNIQUE(name)
-)
--- sql/products/insert-products.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO products(name, price, discount) VALUES ('apple', 135, false);
-INSERT INTO products(name, price, discount) VALUES ('orange', 55, true);
-INSERT INTO products(name, price, discount) VALUES ('kiwi', 165, true);
-INSERT INTO products(name, price, discount) VALUES ('banana', 035, false);
